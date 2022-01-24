90b62567ba16dbaf1c9b54870dddd0c2e9810a64
Ashwin-vemula <ashwin.vemula@toppr.com>
2022-01-24 14:08:55 +0530

    htaccess

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -13,33 +13,7 @@ export default function Home() {
     return (
       <div className={style.container}>
             <Carousel autoplay={false}  >
-              <div className={style.section1}>
-                <div className={style.contentWrapper}>
-                    <div className={style.caroContentBody}>
-                      <div className={style.caroContentHeading}>IAM School</div>
-                      <div className={style.caroContentDesc}>
-                      Digitaiken is digital solution company working on product Development, digital workplace solution and Identity and Access management.
-                      </div>
-                      <div className={style.dFlex}>
-                        <div className={style.gunPointWrapper}>
-                            <img className={style.icon} src={`${process.env.PUBLIC_URL}/images/calendar.svg`} alt="clock" />
-                          <div className={style.gunpointText}>5 Weeks</div>
-                        </div>
-                        <div className={style.gunPointWrapper}>
-                            <img className={style.icon} src={`${process.env.PUBLIC_URL}/images/clock.svg`} alt="calendar" />                    
-                          <div className={style.gunpointText}>26, Feb 2022</div>
-                        </div>
-                      </div>
-                      <div className={style.ctaWrapper}>
-                          <button className={style.primaryCta} onClick={()=> navigate('/course')} >Get Started Now</button>
-                          {/* <button className={style.secondaryCta} >Demo Class</button> */}
-                      </div>
-                    </div>
-                  <div className={style.videoContet}>
-                      <iframe class="mw-youtube-video" width="100%" height="100%" src="https://www.youtube.com/embed/rzl_yG5ofbY?enablejsapi=1&version=3&playerapiid=ytplayer" modestbranding="0"  controls="0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-                  </div>
-                </div>
-              </div>
+              
               <div className={style.section1}>
                 <div className={style.contentWrapper}>
                     <div className={style.caroContentBody}>
@@ -92,6 +66,33 @@ export default function Home() {
                   <div className={style.videoContet}>
                   <iframe class="mw-youtube-video" width="100%" height="100%" src="https://www.youtube.com/embed/0whdVuoLLNs?enablejsapi=1&version=3&playerapiid=ytplayer" modestbranding="0"  controls="0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 
+                  </div>
+                </div>
+              </div>
+              <div className={style.section1}>
+                <div className={style.contentWrapper}>
+                    <div className={style.caroContentBody}>
+                      <div className={style.caroContentHeading}>IAM School</div>
+                      <div className={style.caroContentDesc}>
+                      Digitaiken is digital solution company working on product Development, digital workplace solution and Identity and Access management.
+                      </div>
+                      <div className={style.dFlex}>
+                        <div className={style.gunPointWrapper}>
+                            <img className={style.icon} src={`${process.env.PUBLIC_URL}/images/calendar.svg`} alt="clock" />
+                          <div className={style.gunpointText}>5 Weeks</div>
+                        </div>
+                        <div className={style.gunPointWrapper}>
+                            <img className={style.icon} src={`${process.env.PUBLIC_URL}/images/clock.svg`} alt="calendar" />                    
+                          <div className={style.gunpointText}>26, Feb 2022</div>
+                        </div>
+                      </div>
+                      <div className={style.ctaWrapper}>
+                          <button className={style.primaryCta} onClick={()=> navigate('/course')} >Get Started Now</button>
+                          {/* <button className={style.secondaryCta} >Demo Class</button> */}
+                      </div>
+                    </div>
+                  <div className={style.videoContet}>
+                      <iframe class="mw-youtube-video" width="100%" height="100%" src="https://www.youtube.com/embed/rzl_yG5ofbY?enablejsapi=1&version=3&playerapiid=ytplayer" modestbranding="0"  controls="0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                   </div>
                 </div>
               </div>
